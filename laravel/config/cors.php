a8e5d7afe068a714cad9cfa3f88440a9a1d8f5ae
ashok <ashok.byte.singh@gmail.com>
2026-02-01 15:50:37 +0530

    Fix DB Seeders & Migrates

---
+++ laravel/config/cors.php
@@ -0,0 +1,23 @@
+<?php
+
+return [
+
+    'paths' => [
+        'api/*',
+        'sanctum/csrf-cookie',
+    ],
+
+    'allowed_methods' => ['*'],
+
+    'allowed_origins' => [
+        'https://railway-nuxt-laravel-hls-production.up.railway.app',
+    ],
+
+    'allowed_headers' => ['*'],
+
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+];
