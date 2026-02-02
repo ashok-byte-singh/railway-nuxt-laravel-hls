9e3608f8b5782289fcb54db9c8159d26cd026ecf
ashok <ashok.byte.singh@gmail.com>
2026-02-03 01:16:16 +0530

    update videocontroller update

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -3,6 +3,8 @@
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\VideoController;
+use App\Models\Experiment;
+use App\Http\Controllers\Api\AuthController;
 
 
 // use App\Http\Controllers\Api\AuthController;
@@ -22,3 +24,27 @@ Route::get(
     [VideoController::class, 'segment']
 )->where('file', '.*');
 
+
+Route::post('/login', [AuthController::class, 'login']);
+Route::post('/logout', [AuthController::class, 'logout'])
+    ->middleware('auth:sanctum');
+
+Route::get('/me', fn (Request $r) => $r->user())
+    ->middleware('auth:sanctum');
+   
+    Route::get('/hls/segment/{experiment}/{segment}', function (
+        Experiment $experiment,
+        string $segment
+    ) {
+        $path = "experiments/{$experiment->id}/{$segment}";
+    
+        if (!Storage::disk('s3')->exists($path)) {
+            abort(404, 'Segment not found in bucket');
+        }
+    
+        return response(
+            Storage::disk('s3')->get($path),
+            200,
+            ['Content-Type' => 'video/mp2t']
+        );
+    });
