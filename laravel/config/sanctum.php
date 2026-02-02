b037f1113249649fe732a5d1cd68fc262c37cebe
ashok <ashok.byte.singh@gmail.com>
2026-02-02 15:22:03 +0530

    update sanctum

--- laravel/config/sanctum.php
+++ laravel/config/sanctum.php
@@ -17,7 +17,7 @@ return [
 
     'stateful' => explode(',', env(
     'SANCTUM_STATEFUL_DOMAINS',
-    'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1'
+    'localhost,localhost:3000,127.0.0.1,127.0.0.1:8000,::1,railway-nuxt-laravel-hls-production.up.railway.app'
 )),
 
 
