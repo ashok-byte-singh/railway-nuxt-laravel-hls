c5d57a1ac3a780bb1b0eecfeb4c34d556f420f8b
ashok <ashok.byte.singh@gmail.com>
2026-02-03 02:10:37 +0530

    update videocontroller update

--- laravel/app/Providers/CookieServiceProvider.php
+++ laravel/app/Providers/CookieServiceProvider.php
@@ -2,34 +2,14 @@
 
 namespace App\Providers;
 
-use Illuminate\Support\Facades\Cookie;
 use Illuminate\Support\ServiceProvider;
-use Symfony\Component\HttpFoundation\Cookie as SymfonyCookie;
+use Illuminate\Support\Facades\Config;
 
 class CookieServiceProvider extends ServiceProvider
 {
     public function boot(): void
     {
-        Cookie::macro('session', function (
-            string $name,
-            string $value,
-            int $minutes = 120,
-            string $path = '/',
-            string $domain = null,
-            bool $secure = true,
-            bool $httpOnly = true
-        ) {
-            return new SymfonyCookie(
-                $name,
-                $value,
-                time() + ($minutes * 60),
-                $path,
-                $domain,
-                $secure,
-                $httpOnly,
-                false,
-                'none' // 👈 FORCE SameSite=None
-            );
-        });
+        Config::set('session.same_site', 'none');
+        Config::set('session.secure', true);
     }
 }
